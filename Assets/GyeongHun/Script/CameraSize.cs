57086e200675690234b04e752df6520d26b7b683
wkdrudgnsdla <s24032@gsm.hs.kr>
2025-03-28 16:13:29 +0900

    카메라 끝

--- Assets/GyeongHun/Script/CameraSize.cs
+++ Assets/GyeongHun/Script/CameraSize.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class CameraSize : MonoBehaviour
 {
+    public Vector3 addPos = new Vector3(0, 3, 0);
+
     public Transform object1;
     public Transform object2;
     public float margin = 2f;
@@ -16,6 +18,10 @@ public class CameraSize : MonoBehaviour
 
     void Update()
     {
+        Vector3 pos = new Vector3((object1.position.x + object2.position.x) / 2, (object1.position.y + object2.position.y) / 2, -10);
+
+        transform.position = pos + addPos;
+
         float distance = Vector2.Distance(object1.position, object2.position);
         Camera.main.orthographicSize = Mathf.Max((distance / 2f) + margin, 7f);
     }
