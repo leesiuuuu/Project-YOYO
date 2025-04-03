c880e9d4299744a6a2f3ed36354682e0008038db
LeeSiu <167082095+leesiuuuu@users.noreply.github.com>
2025-04-03 23:13:31 +0900

    Sound Setting Add
    사운드 조절 추가

--- Assets/SiWoo/Scripts/SoundManager.cs
+++ Assets/SiWoo/Scripts/SoundManager.cs
@@ -1,9 +1,13 @@
 using UnityEngine;
+using UnityEngine.Audio;
+using static Unity.VisualScripting.Member;
 
 public class SoundManager : MonoBehaviour
 {
 	private static SoundManager instance;
 
+	public AudioSource bgSound;
+	public AudioMixer audioMixer;
 	public static SoundManager Instance
 	{
 		get
@@ -24,5 +28,30 @@ public class SoundManager : MonoBehaviour
 			Destroy(gameObject);
 		}
 	}
+	public void SFXPlay(string sfxName, AudioClip clip)
+	{
+		GameObject go = new GameObject(sfxName + "Sound");
+		AudioSource source = go.AddComponent<AudioSource>();
+		source.outputAudioMixerGroup = audioMixer.FindMatchingGroups("SFX")[0];
+		source.clip = clip;
+		source.Play();
 
+		Destroy(go, clip.length);
+	}
+	public void BgSoundPlay(AudioClip clip)
+	{
+		bgSound.clip = clip;
+		bgSound.outputAudioMixerGroup = audioMixer.FindMatchingGroups("Music")[0];
+		bgSound.loop = true;
+		bgSound.volume = 0.1f;
+		bgSound.Play();
+	}
+	public void BGSoundVolume(float val)
+	{
+		audioMixer.SetFloat("MusicVolume", Mathf.Log10(val) * 20);
+	}
+	public void SFXSoundVolume(float val)
+	{
+		audioMixer.SetFloat("SFXVolume", Mathf.Log10(val) * 20);
+	}
 }
