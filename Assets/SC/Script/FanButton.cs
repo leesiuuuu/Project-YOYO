9154dda38f08caf81e1f4e00a21fef3da488b10c
magicJSC <jeonghaejun004@gmail.com>
2025-04-09 20:15:14 +0900

    Reapply "Map"
    This reverts commit efbe225d229fa9b158ebcd9eb19bc74a36bbe0bc.

---
+++ Assets/SC/Script/FanButton.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static UnityEngine.InputManagerEntry;
+
+public class FanButton : MonoBehaviour
+{
+    Animator anim;
+    [SerializeField] GameObject wind;
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.GetComponent<Rigidbody2D>() != null)
+        {
+            wind.SetActive(true);
+            anim.Play("Push");
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.GetComponent<Rigidbody2D>() != null)
+        {
+            wind.SetActive(false);
+            anim.Play("Pull");
+        }
+    }
+}
