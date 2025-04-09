2be7fc5ab8a6d5f95926f01c976bc18d644308d4
magicJSC <jeonghaejun004@gmail.com>
2025-04-09 20:12:55 +0900

    Map

---
+++ Assets/SC/Script/DisappearButton.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisappearButton : MonoBehaviour
+{
+    [SerializeField] GameObject disappearObj;
+
+    Animator anim;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.GetComponent<Rigidbody2D>() != null)
+        {
+            disappearObj.SetActive(false);
+            anim.Play("Push");
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.GetComponent<Rigidbody2D>() != null)
+        {
+            disappearObj.SetActive(true);
+            anim.Play("Pull");
+        }
+    }
+}
