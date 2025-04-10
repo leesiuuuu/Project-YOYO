46300ab4e58f8c364fbc37949425317a525c9191
LeeSiu <167082095+leesiuuuu@users.noreply.github.com>
2025-04-10 17:15:51 +0900

    Main Scene Added
    메인 씬 추가됨

--- Assets/SiWoo/Scripts/LevelLoader.cs
+++ Assets/SiWoo/Scripts/LevelLoader.cs
@@ -6,13 +6,6 @@ public class LevelLoader : MonoBehaviour
 	[SerializeField]
 	private Animator animator;
 
-	private void Update()
-	{
-		if (Input.GetKeyDown(KeyCode.Space))
-		{
-			LoadNextLevel(0, true);
-		}
-	}
 	public void LoadNextLevel(int SceneIndex, bool NextLevelLoader = false)
 	{
 		StartCoroutine(LoadLevel(
