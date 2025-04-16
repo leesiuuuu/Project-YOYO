1ddd194846d6895e3777b16efea864a89d6283e5
LeeSiu <167082095+leesiuuuu@users.noreply.github.com>
2025-04-16 16:55:48 +0900

    StageSelector making
    스테이지 선택 만드는 중

---
+++ Assets/SiWoo/Scripts/StageSelector_Child.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageSelector_Child : MonoBehaviour
+{
+	public string StageName;
+	private Animator animator;
+	private bool isSelected = false;
+
+
+}
