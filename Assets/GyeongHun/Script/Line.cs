3b328078df5b5e975d10b18aed275bda73501cc6
wkdrudgnsdla <s24032@gsm.hs.kr>
2025-05-20 11:29:21 +0900

    Grab Pos fix, add LinePos in prefab

--- Assets/GyeongHun/Script/Line.cs
+++ Assets/GyeongHun/Script/Line.cs
@@ -5,12 +5,12 @@ using UnityEngine;
 public class Line : MonoBehaviour
 {
     LineRenderer line;
-    public Transform Player;
+    public Transform TargetGO;
 
     private void Awake()
     {
         line = GetComponent<LineRenderer>();
-        Player = transform.parent.GetComponent<Transform>();
+        //TargetGO = transform.Find("LinePos");
     }
 
     private void Start()
@@ -20,7 +20,7 @@ public class Line : MonoBehaviour
 
     private void Update()
     {
-        line.SetPosition(0, Player.transform.position);
+        line.SetPosition(0, TargetGO.transform.position);
         line.SetPosition(1, gameObject.transform.position);
     }
 }
